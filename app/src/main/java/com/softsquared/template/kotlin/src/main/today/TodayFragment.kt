f5c090cc624665780795f21ab65ffadc5d069a9a
bingotop <wodnjs3600@naver.com>
2021-03-03 15:11:03 +0900

    today_recycer_view_update

--- app/src/main/java/com/softsquared/template/kotlin/src/main/today/TodayFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/today/TodayFragment.kt
@@ -2,15 +2,34 @@ package com.softsquared.template.kotlin.src.main.today
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.config.BaseFragment
 import com.softsquared.template.kotlin.databinding.FragmentTodayBinding
+import com.softsquared.template.kotlin.src.main.today.adapter.MemoAdapter
+import com.softsquared.template.kotlin.src.main.today.models.MemoItem
 
 class TodayFragment :
     BaseFragment<FragmentTodayBinding>(FragmentTodayBinding::bind, R.layout.fragment_today) {
+    private val memoList:ArrayList<MemoItem> = arrayListOf()
+    lateinit var todayMemoAdapter:MemoAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        showCustomToast("aaaaaa")
+        // 더미데이터로 리사이클러뷰 테스트
+        for(i in 1..5){
+            memoList.add(MemoItem(i,"202021",i,"제목","내용",false,"BLUE"))
+        }
+
+        // 어댑터
+        todayMemoAdapter = MemoAdapter(memoList)
+        binding.todayRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = todayMemoAdapter
+        }
+
+
+
     }
 }
